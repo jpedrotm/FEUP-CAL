c72de451a3268ad0186a38279c2a454fb7c3d51a
jpedrotm <j.pedroteixeira.monteiro@gmail.com>
2016-04-27 18:12:57 +0100

    doxy

--- src/ReaderFiles.h
+++ src/ReaderFiles.h
@@ -13,24 +13,95 @@
 #include "Relation.h"
 
 using namespace std;
-
+/**
+ * ReadeFiles-Class responsible for read the files that contains the information of the nodes and the roads.
+ */
 class ReaderFiles{
+	/**
+	 * Private attribute which stores all the nodes.
+	 */
 	vector<Node> nodes;
+	/**
+	 * Private attribute which stores all the roads.
+	 */
 	vector<Road> roads;
+	/**
+	 * Private attribute which stores all the relations between the nodes and the roads.
+	 */
 	vector<Relation> relations;
-	double minLat,maxLat,minLon,maxLon;
+	/**
+	 * Private attribute which stores the minimum latitude.
+	 */
+	double minLat;
+	/**
+	 * Private attribute which stores the maximum latitude.
+	 */
+	double maxLat;
+	/**
+	 * Private attribute which stores the minimum longitude.
+	 */
+	double minLon;
+	/**
+	 * Private attribute which stores the maximum latitude.
+	 */
+	double maxLon;
 public:
+	/**
+	 * Constructor of the class.
+	 */
 	ReaderFiles();
+	/**
+	 * Function which read the file that got all the information of the nodes.
+	 */
 	void readFileNameNodes();
+	/**
+	 * Function which read the file that got all the information of the roads.
+	 */
 	void readFileNameRoads();
+	/**
+	 * Function which read the file with all the relations between the nodes and the roads.
+	 */
 	void readFileNameRelation();
+	/**
+	 * Getter which returns the vector with all the nodes.
+	 * @return nodes.
+	 */
 	vector<Node> getNodes() const;
+	/**
+	 *  Getter which returns the vector with all the roads.
+	 * @return roads.
+	 */
 	vector<Road> getRoads() const;
+	/**
+	 *  Getter which returns the vector with all the relations.
+	 * @return relations.
+	 */
 	vector<Relation> getRelations() const;
+	/**
+	 *  Getter which returns the minimum latitude.
+	 * @return minLat.
+	 */
 	double getMinLat() const;
+	/**
+	 *  Getter which returns the maximum latitude.
+	 * @return maxLat.
+	 */
 	double getMaxLat() const;
+	/**
+	 *  Getter which returns the minimum longitude.
+	 * @return minLon.
+	 */
 	double getMinLon() const;
+	/**
+	 *  Getter which returns the maximum longitude.
+	 * @return maxLon.
+	 */
 	double getMaxLon() const;
+	/**
+	 * Updates the private attributes minLat,miLon,maxLat and maxLon.
+	 * @param lat.
+	 * @param lon.
+	 */
 	void updateLatsAndLons(double lat,double lon);
 
 };
--- src/RideCenter.h
+++ src/RideCenter.h
@@ -9,17 +9,51 @@
 #include "Adress.h"
 #include "GraphViewer.h"
 
-
-class RideCenter{
-	Graph<Node,Road> graph;
+/**
+ * RideCenter-Class that represents all the logic of the program.
+ */
+class RideCenter {
+	/**
+	 * Private attribute that saves all the information in a graph.
+	 */
+	Graph<Node, Road> graph;
+	/**
+	 * Private attribute which stores all the relations between the nodes and the roads.
+	 */
 	vector<Relation> rels;
+	/**
+	 * Private attribute which stores all the nodes.
+	 */
 	vector<Node> nodes;
+	/**
+	 * Private attribute which stores all the roads.
+	 */
 	vector<Road> roads;
+	/**
+	 * Private attribute of type ReaderFiles.
+	 */
 	ReaderFiles reader;
 public:
+	/**
+	 * Constructor of the class with no parameters.
+	 */
 	RideCenter(){};
+	/**
+	 * Constructor of the class that initialize all the private attributes.
+	 * @param r.
+	 */
 	RideCenter(ReaderFiles &r);
+	/**
+	 * Function that prints all the ids's of the nodes in the graph.
+	 */
 	void printGraph() const;
+	/**
+	 * Function that calculates if a node is inside of the elipse.
+	 * @param foco1.
+	 * @param foco2.
+	 * @param n.
+	 * @return true if the node n is inside of the elipse.
+	 */
 	bool in_elipse(Node foco1, Node foco2, Node N);
 
 	//VAI PARA PRIVATE MUDAR DEPOIS
